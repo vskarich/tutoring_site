011453d6f4138c3365f3d30532d27d96e633ecd3
vskarich <vskarich@twitter.com>
2013-04-07 12:37:40 -0700

    added Score and Test

--- sat_test/admin.py
+++ sat_test/admin.py
@@ -1,20 +1,6 @@
+from sat_test.models import Test
+from sat_test.models import Score
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 
-from sat_test.models import Test, Score
-
-# Define an inline admin descriptor for UserProfile model
-# which acts a bit like a singleton
-class TestInline(admin.StackedInline):
-    model = Test
-    can_delete = False
-    verbose_name_plural = 'test'
-
-# Define a new User admin
-class TestAdmin(UserAdmin):
-    inlines = (UserProfileInline, )
-
-# Re-register UserAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(Test)
+admin.site.register(Score)
--- tutoring_site/settings.py
+++ tutoring_site/settings.py
@@ -135,6 +135,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'accounts',
+    'sat_test'
 )
 
 AUTH_PROFILE_MODULE = 'accounts.UserProfile'
